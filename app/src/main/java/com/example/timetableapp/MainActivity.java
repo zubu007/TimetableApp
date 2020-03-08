39e0e82c2df9be4663e397415703a9c05ef8e18c
zubu007 <zubayerhossain111@gmail.com>
2020-03-08 22:06:38 +0600

    finished structure of mainactivity. created initial empty activity for 2nd and 3rd activity. started to set instances of all buttons. started to set onclicklistener of all buttons.

--- app/src/main/java/com/example/timetableapp/MainActivity.java
+++ app/src/main/java/com/example/timetableapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.timetableapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +9,65 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int a;
 
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        a = 0;
 
+        Button btn601 = findViewById(R.id.btn600am_1);
+        Button btn602 = findViewById(R.id.btn600am_2);
+        Button btn603;
+        Button btn604;
+        Button btn605;
+        Button btn606;
+        Button btn607;
+        Button btn651;
+        Button btn652;
+        Button btn653;
+        Button btn654;
+        Button btn655;
+        Button btn656;
+        Button btn657;
+        Button btn701;
+        Button btn702;
+        Button btn703;
+        Button btn704;
+        Button btn705;
+        Button btn706;
+        Button btn707;
+        Button btn751;
+        Button btn752;
+        Button btn753;
+
+
+        btn601.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, inputActivity.class);
+                startActivity(intent);
+
+                a = 601;
+
+
+            }
+        });
+
+        btn602.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, inputActivity.class);
+                startActivity(intent);
+
+                a = 602;
+            }
+        });
 
 
     }
